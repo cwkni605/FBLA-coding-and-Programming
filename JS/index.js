a267698105cddd11b19121b5d5437619a5d639da
Connor Knight <cwkni605@gmail.com>
2020-09-24 13:09:19 -0700

    printing out non functional questions
    the questions now print out but does not validate yet. nor does it persist.

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
+var questionType = 
+{
+    check:"<>"
+}
+
 var questionList = 
 [
-    ["1this is a question test tralse or frue","check","tralse","frue"],
+    ["1this is a question test tralse or frue","check","tralse","frue",],
     ["1this is a question test good, bad or ugly","select","good","bad","ugly"],
     ["1this is a question test tralse or frue","check","tralse","frue"],
     ["1this is a question test tralse or frue","check","tralse","frue"],
@@ -19,16 +24,42 @@ var questionList =
     ["4this is a question test good, bad or ugly","select","good","bad","ugly"],
     ["4this is a question test tralse or frue","check","tralse","frue"],
     ["4this is a question test tralse or frue","check","tralse","frue"],
-    ["4this is a question test tralse or frue","check","tralse","frue"],
+    ["4this is a question test tralse or frue","check","tralse","frue"]
 ];
+
+var answerList = 
+[
+    [1,2],
+    [2],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+]
 
 var form = document.getElementById("form");
 var used = [];
-
-for (let Question = 0; Question < 5; Question++) {
-    var num = Math.round(Math.random()*20);
-    var notUsed = false;
-    for (let i = 0; notUsed == true; i++) {
+var notUsed = false;
+function diminishingRandNum()
+{
+    notUsed = false;
+    for (let i = 0; notUsed != true; i++)
+    {
+        var num = Math.floor(Math.random()*20);
         if(num != used[0] || used[0] == undefined)
         {
             if(num != used[1] || used[1] == undefined)
@@ -39,17 +70,75 @@ for (let Question = 0; Question < 5; Question++) {
                     {
                         if(num != used[4] || used[4] == undefined)
                         {
+                            used.push(num);
                             notUsed = true;
+                            return num;
                         }
                     }
                 }
             }
         }
-        else
+    }
+}
+
+for (let i = 0; i < 5; i++)
+{
+    var questionNumber = diminishingRandNum();
+    var form = document.getElementsByTagName("form")[0];
+    var fieldset = document.createElement("fieldset");
+
+    for (let Index = 0; Index < questionList[questionNumber].length; Index++)
+    {
+        const temp = questionList[questionNumber][Index];
+        if(Index == 0)
         {
-            
+            //question
+            var tempLabel = document.createElement("h2");
+            tempLabel.appendChild(document.createTextNode(temp));
+            fieldset.appendChild(tempLabel);
+        }
+        else if(Index == 1)
+        {
+            //type
+        }
+        //else if(Index == 2)
+        //{
+            //correct answer
+            //answer = temp;
+        //}
+        else if(Index > 1)
+        {
+            //questions
+            if (questionList[questionNumber][1] == "check")
+            {
+                var tempQuestion = document.createElement("input");
+                tempQuestion.type = "checkbox";
+                tempQuestion.name = questionNumber;
+                var tempLabel = document.createElement("label");
+                tempLabel.appendChild(document.createTextNode(temp));
+                fieldset.appendChild(tempLabel);
+                fieldset.appendChild(tempQuestion);
+            }
+            else if(questionList[questionNumber][1] == "select")
+            {
+                var tempQuestion = document.createElement("input");
+                tempQuestion.type = "radio";
+                tempQuestion.required = true;
+                tempQuestion.name = questionNumber;
+                var tempLabel = document.createElement("label");
+                tempLabel.appendChild(document.createTextNode(temp));
+                fieldset.appendChild(tempLabel);
+                fieldset.appendChild(tempQuestion);
+            }
         }
     }
-    const question = questionList[num];
-    console.log(Question);
+    document.getElementsByTagName("form")[0].appendChild(fieldset);
+}
+var tempSubmit = document.createElement("button");
+tempSubmit.type = "submit";
+tempSubmit.appendChild(document.createTextNode("Submit"));
+document.getElementsByTagName("form")[0].appendChild(tempSubmit);
+
+function log(out) {
+    console.log(out);
 }

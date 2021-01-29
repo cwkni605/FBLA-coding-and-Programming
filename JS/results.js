96c3e5e4b3d27c0680e068ac2f8cdf3247c02059
Connor Knight <cwkni605@gmail.com>
2021-01-29 15:22:23 -0700

    small changes

--- JS/results.js
+++ JS/results.js
@@ -1,8 +1,12 @@
+var post = location.search;
 if (typeof(Storage) !== "undefined") {
     // Store
-    //localStorage.setItem("lastname", "Smith");
+    //localStorage.setItem("testSaves", localStorage.getItem("testSaves")+"<h1>FBLA test 1</h1><ul><li>this is question one<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li><li>this is question two<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li><li>this is question three<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li></ul>");
+    //localStorage.setItem("testSaves", null);
     // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("firstname");
+    document.getElementById("result").innerHTML = localStorage.getItem("testSaves");
+    console.log(localStorage.getItem(post));
+    console.log(localStorage.getItem("lastname"));
 }
 else
 {
@@ -10,7 +14,7 @@ else
     {
         if(confirm("Your Browser does NOT allow for site storage. Click ok to return to the menu."))
         {
-            location.replace("index.html");
+            location.assign("index.html");
         }
     }
 }

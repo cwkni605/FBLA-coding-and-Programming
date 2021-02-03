0e51237dc89dec811115f57e65ba0099f199742d
Connor Knight <cwkni605@gmail.com>
2021-02-03 15:23:54 -0700

    results page developement started

---
+++ JS/data.js
@@ -0,0 +1,71 @@
+console.log("data start");
+var questionList = 
+[
+    ["1 this is a question test tralse or frue","check","tralse","frue",],
+    ["2 this is a question test good, bad or ugly","radio","good","bad","ugly"],
+    ["3 this is a question test tralse or frue","radio","tralse","frue"],
+    ["4 this is a question test tralse or frue","check","tralse","frue"],
+    ["5 this is a question test tralse or frue","check","tralse","frue"],
+    ["6 this is a question test tralse or frue","check","tralse","frue"],
+    ["7 this is a question test good, bad or ugly","radio","good","bad","ugly"],
+    ["8 this is a question test tralse or frue","check","tralse","frue"],
+    ["9 this is a question test tralse or frue","check","tralse","frue"],
+    ["10 this is a question test tralse or frue","check","tralse","frue"],
+    ["11 this is a question test tralse or frue","check","tralse","frue"],
+    ["12 this is a question test good, bad or ugly","radio","good","bad","ugly"],
+    ["13 this is a question test tralse or frue","check","tralse","frue"],
+    ["14 this is a question test tralse or frue","check","tralse","frue"],
+    ["15 this is a question test tralse or frue","text","Answer:"]
+];
+
+var answerList = 
+[
+    [1,2],
+    [2],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1]
+]
+
+if (questionList.length !== answerList.length)
+{
+    if (confirm("The question and answer lists do NOT match. Do you want to reload the page?"))
+    {
+        location.reload();
+    }
+}
+
+var used = [];
+function diminishingRandNum(number, limit)
+{
+    var notUsed = false;
+    for (let i = 0; notUsed != true; i++)
+    {
+        var num = Math.floor(Math.random() * questionList.length);
+        if(used.find((trying)=>{return trying === num;}) === undefined)
+        {
+            notUsed = true;
+        }
+    }
+    used.push(num);
+    notUsed = true;
+    if(number !== undefined)
+    {
+        while (number < used.length)
+        {
+            used.shift();
+        }
+    }
+    return num;
+}
+console.log("Data End");
--- JS/results.js
+++ JS/results.js
@@ -1,12 +1,35 @@
 var post = location.search;
+console.log("step 1");
+if(post !== undefined){
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        if(!localStorage.getItem("testSaves").startsWith("null")){
+            localStorage.setItem("testSaves", localStorage.getItem("testSaves")+post);
+        }
+        else{
+            localStorage.setItem("testSaves", post);
+        }
+        //localStorage.setItem("testSaves", null);
+    }
+    else
+    {
+        while(true)
+        {
+            if(confirm("Your Browser does NOT allow for site storage. Click ok to return to the menu."))
+            {
+                location.assign("index.html");
+            }
+        }
+    }
+}
+console.log("step 2")
+var answerSheets;
 if (typeof(Storage) !== "undefined") {
-    // Store
-    //localStorage.setItem("testSaves", localStorage.getItem("testSaves")+"<h1>FBLA test 1</h1><ul><li>this is question one<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li><li>this is question two<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li><li>this is question three<ul><li>answer 1</li><li>answer 2</li><li>answer 3</li><li>answer 4</li></ul></li></ul>");
-    //localStorage.setItem("testSaves", null);
     // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("testSaves");
-    console.log(localStorage.getItem(post));
-    console.log(localStorage.getItem("lastname"));
+    answerSheets = localStorage.getItem("testSaves").split("?");
+    answerSheets.shift();
+    console.log(answerSheets);
+    //console.log(localStorage.getItem("lastname"));
 }
 else
 {
@@ -18,3 +41,69 @@ else
         }
     }
 }
+console.log("step 3");
+console.log(answerSheets);
+for (let I = 0; I < answerSheets.length; I++) {
+    console.log("I:"+I);
+    const element = answerSheets[I];
+    var tempTitle = document.createElement("legend");
+    tempTitle.appendChild(document.createTextNode(`Attempt: ${I}:`));
+    var tempForm = document.createElement('form');
+    tempForm.appendChild(tempTitle);
+    document.body.appendChild(tempForm);
+    for (let i = 0; i < 5; i++)
+    {
+        console.log("i:"+i);
+        var questionNumber = diminishingRandNum();
+        var form = document.getElementsByTagName("form")[I];
+        var fieldset = document.createElement("fieldset");
+
+        for (let Index = 0; Index < questionList[questionNumber].length; Index++)
+        {
+            console.log("Index:"+Index);
+            const temp = questionList[questionNumber][Index];
+            if(Index == 0)
+            {
+                //question
+                var tempLabel = document.createElement("h2");
+                tempLabel.appendChild(document.createTextNode(`${i+1}. `+temp));
+                fieldset.appendChild(tempLabel);
+            }
+            else if(Index == 1)
+            {
+                //type
+            }
+            else if(Index > 1)
+            {
+                var tempQuestion = document.createElement("input");
+                tempQuestion.name = questionNumber+"-"+i;
+                tempQuestion.disabled = true;
+                var tempLabel = document.createElement("label");
+                tempLabel.appendChild(document.createTextNode(temp));
+                fieldset.appendChild(tempLabel);
+                //questions
+                if (questionList[questionNumber][1] == "check")
+                {
+                    tempQuestion.type = "checkbox";
+                    
+                }
+                else if(questionList[questionNumber][1] == "radio")
+                {
+                    tempQuestion.type = "radio";
+                }
+                else if(questionList[questionNumber][1] == "text")
+                {
+                    tempQuestion.type = "text";
+                }
+                if(questionList[questionNumber][1] !== "text")
+                {
+                    tempQuestion.value = questionNumber+"-"+i+"-"+(Index-2);
+                }
+                fieldset.appendChild(tempQuestion);
+            }
+            console.log("append question");
+        }
+        document.getElementsByTagName("form")[I].appendChild(fieldset);
+    }
+}
+console.log("step 4");
